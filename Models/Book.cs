55b19b35c8e9b0741e26533787a9620042ac25b1
MarceloTonhetti <marcelotjunior2507@gmail.com>
2021-03-15 00:10:56 -0300

    Implementing Book model

---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class Book
+	{
+		public long TumbleNumber { get; set; }
+		public string ISBN { get; set; }
+		public string Title { get; set; }
+		public string Genre { get; set; }
+		public DateTime PublicationDate { get; set; }
+		public string Author { get; set; }
+
+		public string ToStringForWriteFile()
+		{
+			return $"{TumbleNumber};{ISBN};{Title};{Genre};{PublicationDate};{Author}";
+		}
+	}
+}
