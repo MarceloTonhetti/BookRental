1081215fbbe0d4b155fe699a7bf3d386f430bbde
MarceloTonhetti <marcelotjunior2507@gmail.com>
2021-03-16 01:05:39 -0300

    Changing Console.Write content

--- BookRental/Program.cs
+++ BookRental/Program.cs
@@ -36,7 +36,7 @@ namespace BookRental
 				}
 			} while (option != 0);
 
-			Console.Write("Pressione qualquer tecla para encerrar o sistema...");
+			Console.Write("\nPressione qualquer tecla para encerrar o sistema...");
 			Console.ReadKey();
 		}
 
@@ -46,18 +46,17 @@ namespace BookRental
 			string aux;
 			int option;
 			Console.Clear();
-			
-				Console.WriteLine("============= Biblioteca Municipal =============");
-				Console.WriteLine("================ Menu Principal ================");
-				Console.WriteLine("1 - Cadastro de Cliente");
-				Console.WriteLine("2 - Cadastro de Livro");
-				Console.WriteLine("3 - Emprestimo de Livro");
-				Console.WriteLine("4 - Devolucao do Livro");
-				Console.WriteLine("5 - Relatorio de Emprestimos e Devolucoes");
-				Console.WriteLine("0 - Fechar Sistema");
+			Console.WriteLine("============= Biblioteca Municipal =============");
+			Console.WriteLine("================ Menu Principal ================");
+			Console.WriteLine("1 - Cadastro de Cliente");
+			Console.WriteLine("2 - Cadastro de Livro");
+			Console.WriteLine("3 - Emprestimo de Livro");
+			Console.WriteLine("4 - Devolucao do Livro");
+			Console.WriteLine("5 - Relatorio de Emprestimos e Devolucoes");
+			Console.WriteLine("0 - Fechar Sistema");
 			do
 			{
-				Console.Write("Escolha uma opcao: ");
+				Console.Write("\nEscolha uma opcao: ");
 				aux = Console.ReadLine();
 				if (!int.TryParse(aux, out option) || (option < 0) || (option > 5))
 					Console.WriteLine("Opcao invalida, escolha entre os numeros de 0 a 5");
